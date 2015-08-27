81ebe12c521226afe234fe928b713db7280d6246
Jeff <pastorjcox@gmail.com>
2015-08-26 20:25:43 -0500

    basic 404

--- famstart.py
+++ famstart.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 @app.route('/famstart/')
 def start(app='FamStart'):
     return render_template('famstart.html',app=app)
